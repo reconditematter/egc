957f47d7e0bb96b339f96847c24dbc64667f85c9
Leonid Kneller <recondite.matter@gmail.com>
2021-01-21 16:19:43 -0500

    Implement `ParCH2`, a version of `CH2` that executes multiple goroutines in parallel.

--- ch2.go
+++ ch2.go
@@ -3,6 +3,7 @@ package egc
 import (
 	"github.com/reconditematter/cds"
 	"sort"
+	"sync"
 )
 
 // CH2 -- computes the convex hull of a collection of points in the plane.
@@ -84,6 +85,61 @@ func CH2(ps []Point2) (lower, upper []Point2) {
 	for i := range upper {
 		upper[i] = upperhull.Get(i).(Point2)
 	}
+	return
+}
+
+// ParCH2 -- computes the convex hull of a collection of points in the plane.
+// The function implements Graham's scan algorithm with Andrew's modification.
+// It computes both the lower hull and the upper hull. The hull vertices are listed in
+// counter-clockwise order. The function modifies the input `ps` by reordering it.
+// The implementation uses `ncpu` goroutines running concurrently to accelerate
+// the computations.
+//
+// Reference: R.L. Graham, An efficient algorithm for determining the convex hull of a
+// finite planar set, Inform. Process. Lett., 1:132-133 (1972).
+//
+// DOI: https://dx.doi.org/10.1016/0020-0190(72)90045-2
+//
+// Reference: A.M. Andrew, Another efficient algorithm for convex hulls in two dimensions,
+// Inform. Process. Lett., 9:216-219 (1979).
+//
+// DOI: https://dx.doi.org/10.1016/0020-0190(79)90072-3
+func ParCH2(ncpu int, ps []Point2) (lower, upper []Point2) {
+	if ncpu < 1 {
+		ncpu = 1
+	}
+	if ncpu > 16 {
+		ncpu = 16
+	}
+	n := len(ps)
+	if n < ncpu {
+		// Use the sequential algorithm.
+		lower, upper = CH2(ps)
+		return
+	}
+	//
+	// Parallel loop.
+	//
+	var mu sync.Mutex
+	var wg sync.WaitGroup
+	coll := make([]Point2, 0)
+	for cpu := 0; cpu < ncpu; cpu++ {
+		wg.Add(1)
+		first, limit := cpu*n/ncpu, (cpu+1)*n/ncpu
+		go func(first, limit int) {
+			lowerhull, upperhull := CH2(ps[first:limit])
+			mu.Lock()
+			coll = append(coll, lowerhull...)
+			coll = append(coll, upperhull...)
+			mu.Unlock()
+			wg.Done()
+		}(first, limit)
+	}
+	wg.Wait()
+	//
+	//
+	//
+	lower, upper = CH2(coll)
 	return
 }
 
